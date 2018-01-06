08ce83ebabcaf25f0ec5080542bfbfa17f7a9623
bonlai <bonlai888@hotmail.com>
2018-01-06 17:02:00 +0800

    upload image from gallery

--- app/src/main/java/com/bonlai/socialdiningapp/ImageUploadActivity.java
+++ app/src/main/java/com/bonlai/socialdiningapp/ImageUploadActivity.java
@@ -1,9 +1,11 @@
 package com.bonlai.socialdiningapp;
 
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -61,16 +63,28 @@ public class ImageUploadActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK) {
-            String imagePath;
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            String imagePath = null;
+            Uri uri = data.getData();
 
-            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-            cursor.moveToFirst();
+            if (DocumentsContract.isDocumentUri(this, uri)) {
+                String docId = DocumentsContract.getDocumentId(uri);
+                if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                    //Log.d(TAG, uri.toString());
+                    String id = docId.split(":")[1];
+                    String selection = MediaStore.Images.Media._ID + "=" + id;
+                    imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+                } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                    //Log.d(TAG, uri.toString());
+                    Uri contentUri = ContentUris.withAppendedId(
+                            Uri.parse("content://downloads/public_downloads"),
+                            Long.valueOf(docId));
+                    imagePath = getImagePath(contentUri, null);
+                }
+            } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+                //Log.d(TAG, "content: " + uri.toString());
+                imagePath = getImagePath(uri, null);
+            }
 
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            imagePath = cursor.getString(columnIndex);
-            cursor.close();
             //"/storage/emulated/0/DCIM/100ANDRO/DSC_0001.jpg"
             File file = new File(imagePath);
 
@@ -93,5 +107,18 @@ public class ImageUploadActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+
+            cursor.close();
+        }
+        return path;
     }
 }
