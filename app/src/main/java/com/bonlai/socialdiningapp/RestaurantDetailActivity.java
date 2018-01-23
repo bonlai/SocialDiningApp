ecea3595168a5f587d7ffa110e4c47de9053a372
bonlai <bonlai888@hotmail.com>
2018-01-24 00:48:11 +0800

    review comment list

--- app/src/main/java/com/bonlai/socialdiningapp/RestaurantDetailActivity.java
+++ app/src/main/java/com/bonlai/socialdiningapp/RestaurantDetailActivity.java
@@ -1,16 +1,27 @@
 package com.bonlai.socialdiningapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bonlai.socialdiningapp.main_page.MyRestaurantRecyclerViewAdapter;
+import com.bonlai.socialdiningapp.models.Restaurant;
 import com.bonlai.socialdiningapp.models.Review;
 import com.bonlai.socialdiningapp.models.Token;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -20,12 +31,16 @@ import retrofit2.Response;
 
 public class RestaurantDetailActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        initUI();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -37,16 +52,30 @@ public class RestaurantDetailActivity extends AppCompatActivity {
         });
 
         int restaurantId = getIntent().getExtras().getInt("restaurantId");
-        getReview(restaurantId);
+        getReview(this,restaurantId,recyclerView);
+
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+
     }
 
-    private void getReview(int restaurantId){
+    private void initUI(){
+        recyclerView=(RecyclerView)findViewById(R.id.list_view);
+    }
+
+    private void getReview(final Context context,int restaurantId, final RecyclerView recyclerView){
         APIclient.APIService service=APIclient.getAPIService();
         Call<List<Review>> getReview = service.getReview(restaurantId);
         getReview.enqueue(new Callback<List<Review>>() {
             @Override
             public void onResponse(Call<List<Review>> call, Response<List<Review>> response) {
                 if(response.isSuccessful()){
+                    List<Review> review=response.body();
+                    Log.d("array lentgh",""+review.size());
+                    recyclerView.setAdapter(new MyCommentRecyclerViewAdapter(context,review));
                 }
             }
             @Override
@@ -54,6 +83,61 @@ public class RestaurantDetailActivity extends AppCompatActivity {
                 t.printStackTrace();
             }
         });
+    }
+
+    public class MyCommentRecyclerViewAdapter extends RecyclerView.Adapter<MyCommentRecyclerViewAdapter.ViewHolder> {
+
+        private final List<Review> mReview;
+        Context context;
+
+        public MyCommentRecyclerViewAdapter(Context context,List<Review> review) {
+            this.context = context;
+            mReview=review;
+        }
+
+        @Override
+        public MyCommentRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.comment, parent, false);
+            return new MyCommentRecyclerViewAdapter.ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final MyCommentRecyclerViewAdapter.ViewHolder holder, int position) {
+ /*           //String imgPath = mReview.get(position).getImage().get(0).getImage();
+            final int restaurantId=mRestaurant.get(position).getId();
+            Picasso.with(context).load(imgPath).placeholder( R.drawable.progress_animation ).fit().centerCrop().into(holder.mRestaurantImg);
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent (context, RestaurantDetailActivity.class);
+                    intent.putExtra("restaurantId", restaurantId);
+                    context.startActivity(intent);
+                }
+            });*/
+            Log.d("onbind",""+position);
+            holder.mComment.setText(mReview.get(position).getComment());
+        }
+
+        @Override
+        public int getItemCount() {
+            Log.d("itemcount",""+mReview.size());
+            return mReview.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+
+            public TextView mComment;
+            // Restaurant mRestaurant;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mComment=(TextView) mView.findViewById(R.id.comment);
+                //mRestaurantImg=(ImageView) mView.findViewById(R.id.restaurant_img);
+            }
+        }
     }
 
 }
