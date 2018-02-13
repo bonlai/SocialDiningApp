0c3b8aea1b0d0ac0e44a886d8e8e1bd520fe6b18
bonlai <bonlai888@hotmail.com>
2018-02-14 02:06:54 +0800

    Cropped image upload function

--- app/src/main/java/com/bonlai/socialdiningapp/main/ProfileFragment.java
+++ app/src/main/java/com/bonlai/socialdiningapp/main/ProfileFragment.java
@@ -2,17 +2,21 @@ package com.bonlai.socialdiningapp.main;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.ContentUris;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -23,6 +27,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bonlai.socialdiningapp.APIclient;
 import com.bonlai.socialdiningapp.LoginActivity;
@@ -37,8 +42,13 @@ import com.bonlai.socialdiningapp.models.Token;
 import com.bonlai.socialdiningapp.detail.profileEdit.EditBioActivity;
 import com.bonlai.socialdiningapp.models.User;
 import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import okhttp3.MediaType;
@@ -73,6 +83,8 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     private User mUser;
 
     private static final String ARG_MODE = "mode";
+
+    Uri imageUri;
 
     public static enum ProfileMode {
         OTHER,
@@ -208,7 +220,6 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         });
     }
 
-
     @Override
     public void onStop() {
         super.onStop();
@@ -217,31 +228,17 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK) {
-            String imagePath = null;
-            Uri uri = data.getData();
 
-            if (DocumentsContract.isDocumentUri((AppCompatActivity) getActivity(), uri)) {
-                String docId = DocumentsContract.getDocumentId(uri);
-                if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
-                    //Log.d(TAG, uri.toString());
-                    String id = docId.split(":")[1];
-                    String selection = MediaStore.Images.Media._ID + "=" + id;
-                    imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
-                } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
-                    //Log.d(TAG, uri.toString());
-                    Uri contentUri = ContentUris.withAppendedId(
-                            Uri.parse("content://downloads/public_downloads"),
-                            Long.valueOf(docId));
-                    imagePath = getImagePath(contentUri, null);
-                }
-            } else if ("content".equalsIgnoreCase(uri.getScheme())) {
-                //Log.d(TAG, "content: " + uri.toString());
-                imagePath = getImagePath(uri, null);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == Activity.RESULT_OK) {
+                Uri resultUri = result.getUri();
+                File imageFile = new File(resultUri.getPath());
+                postImage(imageFile);
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
             }
-            File file = new File(imagePath);
-
-            postImage(file);
         }
     }
 
@@ -266,34 +263,24 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         });
     }
 
-    private String getImagePath(Uri uri, String selection) {
-        String path = null;
-        AppCompatActivity activity = (AppCompatActivity) getActivity();
-        Cursor cursor = activity.getContentResolver().query(uri, null, selection, null, null);
-        if (cursor != null) {
-            if (cursor.moveToFirst()) {
-                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
-            }
-            cursor.close();
-        }
-        return path;
-    }
-
     private void isStoragePermissionGranted() {
         if (Build.VERSION.SDK_INT >= 23) {
+            Log.d("Permission","called");
             int REQUEST_CODE_CONTACT = 101;
-            String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
-            for (String str : permissions) {
+            String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+/*            for (String str : permissions) {
                 if (getActivity().checkSelfPermission(str) != PackageManager.PERMISSION_GRANTED) {
                     this.requestPermissions(permissions, REQUEST_CODE_CONTACT);
                 }
-            }
+            }*/
+            ActivityCompat.requestPermissions(getActivity(),permissions,REQUEST_CODE_CONTACT);
         }
     }
 
     @Override
     public void onClick(View view) {
         Intent intent;
+
         switch (view.getId()) {
             case R.id.bio_holder:
                 intent = new Intent(getContext(), EditBioActivity.class);
@@ -317,11 +304,11 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
                 break;
 
             case R.id.edit_pic:
-                intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Image"), PICK_IMAGE);
+                CropImage.activity()
+                        .setAspectRatio(1,1)
+                        .start(getContext(), this);
                 break;
+
             case R.id.logout:
                 SharedPreferences settings = getActivity().getSharedPreferences(SETTING_INFOS, 0);
                 settings.edit().remove("TOKEN").commit();
